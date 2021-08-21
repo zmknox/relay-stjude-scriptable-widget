83a148ab09fd5c2d7039c899da0126db71736b9e
Zach Knox <zmknox@users.noreply.github.com>
2021-08-21 10:48:19 -0400

    Updated script with milestones

--- Relay St. Jude Progress.js
+++ Relay St. Jude Progress.js
@@ -3,14 +3,21 @@
 // icon-color: yellow; icon-glyph: heart;
 // config
 const width = 320;
-const height = 20;
 const primaryTextColor = new Color("#efefef");
 
 const widget = new ListWidget();
-widget.backgroundColor = new Color("#dcb748");
+
+// widget background
+const gradient = new LinearGradient();
+gradient.colors = [    
+    new Color(`#dcb748`),
+    new Color(`#d6aa29`)
+];
+gradient.locations = [0.0, 1];
+widget.backgroundGradient = gradient;
+
 widget.url = "https://stjude.org/relay";
 
-console.log(Device.locale());
 // api request
 const req = new Request('https://api.tiltify.com');
 req.headers = {    
@@ -81,14 +88,61 @@ amountText.font = Font.heavyRoundedSystemFont(20);
 widget.addSpacer(6);
 
 // progress bar & percentage
-const progressBar = widget.addImage(createProgressBar(total, soFar));
-progressBar.imageSize=new Size(width, height);
+const progressBar = widget.addImage(createProgressBar(total, soFar, 20, true));
+progressBar.imageSize=new Size(width, 20);
+
+widget.addSpacer(4);
+
+// only show as many milestones as the user requests
+// (1 looks good on a medium widget, 3 or 4 on a large)
+let maxMilestones = args.widgetParameter;
+let milestonesDisplayed = 0;
+// progress bars for milestones
+for (let milestone of body.data.campaign.milestones) {
+    if (maxMilestones != undefined && milestonesDisplayed >= maxMilestones) {
+        break;
+    }
+    
+    // calculate milestone percentage
+    const milestoneTotal = parseFloat(milestone.amount.value);
+    const percentage = (soFar / milestoneTotal) * 100;
+    
+    // if the milestone is long past, no need to show it
+    if (percentage < 110) {    
+        widget.addSpacer(6);
+        
+        // a stack will let us put the name and percentage side by side
+        const stack = widget.addStack();
+        stack.spacing = 4;
+    
+        // milestone name
+        const milestoneNameText = stack.addText(milestone.name);
+        milestoneNameText.textColor = primaryTextColor;
+        milestoneNameText.font = Font.boldSystemFont(16);
+        milestoneNameText.lineLimit = 2;
+        
+        // milestone percentage
+        const percentageFixed = percentage.toFixed(2);
+        const percentageText = stack.addText(`${percentageFixed}%`);
+        percentageText.textColor = primaryTextColor;
+        percentageText.font = Font.regularRoundedSystemFont(16);
+        
+        widget.addSpacer(6);
+    
+        // milestone progress bar
+        const milestoneProgressBar = widget.addImage(createProgressBar(milestoneTotal, soFar, 10, false));    
+        milestoneProgressBar.imageSize=new Size(width, 10);
+        
+        milestonesDisplayed += 1;
+    }
+}
+
 
 Script.setWidget(widget);
 Script.complete();
-widget.presentMedium();
+widget.presentLarge();
 
-function createProgressBar(total, soFar){
+function createProgressBar(total, soFar, height, showPercentage = false) {
     const context = new DrawContext();
     context.size = new Size(width, height);
     context.opaque = false;
@@ -97,28 +151,30 @@ function createProgressBar(total, soFar){
     // bar background
     context.setFillColor(new Color("#48484b"));
     const bgPath = new Path();
-    bgPath.addRoundedRect(new Rect(0, 0, width, height), 10, 9);
+    bgPath.addRoundedRect(new Rect(0, 0, width, height), height / 2, (height / 2) - 1);
     context.addPath(bgPath);
     context.fillPath();
     
     // bar foreground
     context.setFillColor(new Color("#00b100"));
     const fgPath = new Path();
-    fgPath.addRoundedRect(new Rect(0, 0, (width * soFar)/total, height), 10, 9);
+    fgPath.addRoundedRect(new Rect(0, 0, (width * soFar)/total, height), height / 2, (height / 2) - 1);
     context.addPath(fgPath);
     context.fillPath();
     
     // percentage text
-    const percentage = ((soFar / total) * 100).toFixed(2);
-    let xPos = (width * soFar)/total + 5;
-    // if over 70%, show in foreground area
-    // to ensure that it doesnt overflow the display
-    if (percentage > 70) {
-        xPos = (width * soFar)/total - 55;
+    if (showPercentage) {
+        const percentage = ((soFar / total) * 100).toFixed(2);
+        let xPos = (width * soFar)/total + 5;
+        // if over 70%, show in foreground area
+        // to ensure that it doesnt overflow the display
+        if (percentage > 70) {
+            xPos = (width * soFar)/total - 55;
+        }
+        context.setFont(Font.semiboldRoundedSystemFont(14));
+        context.setTextColor(primaryTextColor);
+        context.drawText(`${percentage}%`, new Point(xPos, (height / 14)));
     }
-    context.setFont(Font.semiboldRoundedSystemFont(14));
-    context.setTextColor(primaryTextColor);
-    context.drawText(`${percentage}%`, new Point(xPos, (height / 14)));
 
     return context.getImage();
 }
